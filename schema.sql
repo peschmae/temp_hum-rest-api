14a68a2f6ebaf90a556c6214b79d82b9cc06a079
Mathias Petermann <mathias.petermann@gmail.com>
2017-01-15 00:08:29 +0100

    Fix schema.sql, remove debug output

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 drop table if exists temperature;
 create table temperature (
-  id integer primary key autoincrement,
-  temp float not null,
+  id integer primary key auto_increment,
+  temp float not null
 );
+drop table if exists humidity;
 create table humidity (
-  id integer primary key autoincrement,
-  hum float not null,
+  id integer primary key auto_increment,
+  hum float not null
 );
