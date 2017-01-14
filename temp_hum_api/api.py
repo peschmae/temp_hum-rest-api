92ad4afac0b5e57942042cee3efbbd670ffc7a5b
Mathias Petermann <mathias.petermann@gmail.com>
2017-01-14 21:11:06 +0100

    Add requirements.txt and basic API implementation

---
+++ temp_hum_api/api.py
@@ -0,0 +1,68 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse, abort
+
+app = Flask(__name__)
+api = Api(app)
+
+temperature = {
+    1: 20.9,
+    2: 22.2
+}
+humidity = {
+    1: 40.5,
+    2: 30.9
+}
+
+
+def abort_if_record_doesnt_exist(record_id):
+    if record_id not in temperature:
+        abort(404, message="Temperature record {} doesn't exist".format(record_id))
+    if record_id not in humidity:
+        abort(404, message="Temperature record {} doesn't exist".format(record_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('temp', type=float, help='Temperature measured by sensor')
+parser.add_argument('hum', type=float, help='Humidity measured by sensor')
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/', '/hello')
+
+
+class TemperatureHumidity(Resource):
+    def get(self, record_id):
+        abort_if_record_doesnt_exist(record_id)
+        return {record_id: [temperature[record_id], humidity[record_id]]}
+
+    def put(self, record_id):
+        args = parser.parse_args()
+        temp = args['temp']
+        hum = args['hum']
+        abort_if_record_doesnt_exist(record_id)
+        temperature[record_id] = temp
+        humidity[record_id] = hum
+        return {record_id: [temperature[record_id], humidity[record_id]]},201
+
+
+class TemperatureHumidityList(Resource):
+    def get(self):
+        records = {}
+        for key,temp in temperature.iteritems():
+            records.update({key: [temp, humidity[key]]})
+        return records
+
+    def post(self):
+        args = parser.parse_args()
+        record_id = int(max(temperature.keys())) + 1
+        temperature[record_id] = args['temp']
+        humidity[record_id] = args['hum']
+        return {record_id: [temperature[record_id], humidity[record_id]]}, 201
+
+api.add_resource(TemperatureHumidity, '/temp-hum/<int:record_id>', endpoint='temp_hum')
+api.add_resource(TemperatureHumidityList, '/temp-hum-list/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ temp_hum_api/config.py
@@ -0,0 +1,50 @@
+# Builtin config values: http://flask.pocoo.org/docs/0.10/config/
+import os
+import logging
+
+
+class BaseConfig(object):
+    DEBUG = os.environ.get('DEBUG', False)
+    HOST = os.environ.get('HOST', 'localhost')
+    PORT = int(os.environ.get('PORT', 5000))
+
+    LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    LOGGING_LOCATION = 'temp_hum-api.log'
+    LOGGING_LEVEL = logging.DEBUG
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    TESTING = False
+
+
+class TestingConfig(BaseConfig):
+    DEBUG = False
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    TESTING = False
+
+    LOGGING_LEVEL = logging.FATAL
+
+
+config = {
+    "development": "temp_hum_api.config.DevelopmentConfig",
+    "testing": "temp_hum_api.config.TestingConfig",
+    "production": "temp_hum_api.config.ProductionConfig",
+    "default": "temp_hum_api.config.DevelopmentConfig"
+}
+
+
+def configure_app(app):
+    config_name = os.getenv('FLASK_CONFIGURATION', 'default')
+    app.config.from_object(config[config_name])
+    app.config.from_pyfile('config.cfg', silent=True)
+    # Configure logging
+    handler = logging.FileHandler(app.config['LOGGING_LOCATION'])
+    handler.setLevel(app.config['LOGGING_LEVEL'])
+    formatter = logging.Formatter(app.config['LOGGING_FORMAT'])
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
